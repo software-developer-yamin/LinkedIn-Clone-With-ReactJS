362e9e3528785aae3bc522da3f2834cc10981455
DeveloperYamin <developer.yamin@gmail.com>
2022-03-08 01:56:05 +0600

    setup firebase

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth"
+import "firebase/compat/firestore"
+import "firebase/compat/storage"
+
+const firebaseConfig = {
+     apiKey: "AIzaSyD5gRLZ-eBR1KLCE213MKCOuYSQrJQODgk",
+     authDomain: "linkedin-clone-with-reactjs.firebaseapp.com",
+     projectId: "linkedin-clone-with-reactjs",
+     storageBucket: "linkedin-clone-with-reactjs.appspot.com",
+     messagingSenderId: "1014421886260",
+     appId: "1:1014421886260:web:456ae33ee9841e6c930161",
+     measurementId: "G-MGT4PEG9VP"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
