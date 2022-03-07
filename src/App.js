cee923c2a849bdab53f18fcdef278bf821e7e2b3
DeveloperYamin <developer.yamin@gmail.com>
2022-03-07 20:56:37 +0600

    create sidebar structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Sidebar />
     </div>
   );
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -10,6 +10,20 @@ function Sidebar() {
         <h2>Yamin</h2>
         <h4>developer.yamin@gmail.com</h4>
       </div>
+      <div className="sidebar_stats">
+        <div className="sidebar_stat">
+          <p>Who viewed you</p>
+          <p className="sidebar_statNumber">2,543</p>
+        </div>
+
+        <div className="sidebar_stat">
+          <p>Views on post</p>
+          <p className="sidebar_statNumber">2,448</p>
+        </div>
+      </div>
+      <div className="sidebar_bottom">
+        <p>Recent</p>
+      </div>
     </section>
   );
 }
