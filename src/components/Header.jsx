bf0e0d3ae9c5fa1b704f8ff85bc1c5e03be70ed8
DeveloperYamin <developer.yamin@gmail.com>
2022-03-07 12:50:23 +0600

    add material ui icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,18 @@
+import SearchIcon from "@mui/icons-material/Search";
+
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header className="header">
+      <section className="header_right">
+        <img src="" alt="" className="" />
+        <div className="header_search">
+          <SearchIcon />
+          <input type="text" />
+        </div>
+      </section>
+      <section className="header_left"></section>
+    </header>
+  );
 }
 
 export default Header;
