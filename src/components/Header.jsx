3847fc1d2dfde563ce4b8ee3c6a3f9095b47014a
DeveloperYamin <developer.yamin@gmail.com>
2022-03-07 13:12:08 +0600

    header-left style done!

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,18 +4,17 @@ import "../styles/Header.css";
 function Header() {
   return (
     <header className="header">
-      <section className="header_right">
+      <section className="header_left">
         <img
           src="https://cdn-icons.flaticon.com/png/512/3536/premium/3536505.png?token=exp=1646636901~hmac=1dddef82db49e52b32f355d2db03f2f9"
           alt=""
-          className=""
         />
         <div className="header_search">
           <SearchIcon />
           <input type="text" />
         </div>
       </section>
-      <section className="header_left"></section>
+      <section className="header_right"></section>
     </header>
   );
 }
