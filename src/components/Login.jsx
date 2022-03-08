39c292416372a1b6a9e6c67c482cd1f76f4ba8ce
DeveloperYamin <developer.yamin@gmail.com>
2022-03-08 15:55:56 +0600

    Building the Login Page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,90 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { login } from "../features/users/userSlice";
+import { auth } from "../firebase";
 import "../styles/Login.css";
 
 function Login() {
-  return <div>Login</div>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [profilePic, setProfilePic] = useState("");
+  const dispatch = useDispatch();
+
+  const loginToApp = (e) => {
+    e.preventDefault();
+  };
+
+  const register = (e) => {
+    e.preventDefault();
+
+    if (!name) return alert("Please enter your full name!");
+
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((userAuth) => {
+        userAuth.user
+          .updateProfile({
+            displayName: name,
+            photoURL: profilePic,
+          })
+          .then(() => {
+            dispatch(
+              login({
+                email: userAuth.user.email,
+                uid: userAuth.user.uid,
+                displayName: name,
+                profilePic: profilePic,
+              })
+            );
+          });
+      })
+      .catch((err) => alert(err));
+  };
+
+  return (
+    <section className="login">
+      <img
+        src="https://logos-world.net/wp-content/uploads/2020/04/Linkedin-Logo-2011-2019.png"
+        alt=""
+      />
+      <form>
+        <input
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          type="text"
+          placeholder="Full Name (Required if registering)"
+        />
+        <input
+          value={profilePic}
+          onChange={(e) => setProfilePic(e.target.value)}
+          type="text"
+          placeholder="Profile Picture Url (Optional)"
+        />
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          placeholder="Email"
+        />
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          placeholder="Password"
+        />
+        <button type="submit" onClick={loginToApp}>
+          Sign In
+        </button>
+      </form>
+      <p>
+        Not a member?{" "}
+        <span className="login_register" onClick={register}>
+          Registration Now
+        </span>
+      </p>
+    </section>
+  );
 }
 
 export default Login;
