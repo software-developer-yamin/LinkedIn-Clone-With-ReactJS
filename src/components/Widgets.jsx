6003daac98689098ddfb5e13a603dcc26386c985
DeveloperYamin <developer.yamin@gmail.com>
2022-03-08 19:46:11 +0600

    Building the Widgets Section

--- src/components/Widgets.jsx
+++ src/components/Widgets.jsx
@@ -1,7 +1,38 @@
+import { FiberManualRecord, Info } from "@mui/icons-material";
 import "../styles/Widgets.css";
 
 function Widgets() {
-  return <div>Widgets</div>;
+  const newsArticle = (heading, subtitle) => (
+    <div className="widgets_article">
+      <div className="widgets_articleLeft">
+        <FiberManualRecord />
+      </div>
+      <div className="widgets_articleRight">
+        <h4>{heading}</h4>
+        <p>{subtitle}</p>
+      </div>
+    </div>
+  );
+  return (
+    <section className="widgets">
+      <div className="widgets_header">
+        <h2>Linkedin News</h2>
+        <Info />
+      </div>
+      {newsArticle(
+        "All Bangladeshi English Newspapers",
+        "Top views - 100000 readers"
+      )}
+      {newsArticle(
+        "All Bangladeshi English Newspapers",
+        "Top views - 100000 readers"
+      )}
+      {newsArticle(
+        "All Bangladeshi English Newspapers",
+        "Top views - 100000 readers"
+      )}
+    </section>
+  );
 }
 
 export default Widgets;
